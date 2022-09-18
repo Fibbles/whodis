b445356ef85618d9acbc7c7de096409c688c1490
Fibbs <3208355+Fibbles@users.noreply.github.com>
2022-09-18 23:50:40 +0100

    Rough note list

--- gui_settings.lua
+++ gui_settings.lua
@@ -4,7 +4,7 @@ local ADDON_NAME, WHODIS_NS = ...
 
 -- GUI Addon Settings Page
 
-local function create_bool_options(parent_frame, anchor_frame, y_offset)
+local function whodis_create_bool_options(parent_frame, anchor_frame, y_offset)
 
 	-- setters just re-use slash commands since the functionality is already there
 	-- getters are required because any non table value stored directly will be a copy not a reference
@@ -64,7 +64,8 @@ local function create_bool_options(parent_frame, anchor_frame, y_offset)
 	return anchor_frame
 end
 
-local function create_text_options(parent_frame, anchor_frame, y_offset)
+
+local function whodis_create_text_options(parent_frame, anchor_frame, y_offset)
 	
 	local x_padding = 20
 	
@@ -74,11 +75,11 @@ local function create_text_options(parent_frame, anchor_frame, y_offset)
 	label:SetText("Rank Filter")
 	
 	local eb = CreateFrame("EditBox", nil, parent_frame, "InputBoxTemplate")
-    eb:SetSize(200,22)
-    eb:SetAutoFocus(false)
+	eb:SetSize(200, 22)
+	eb:SetAutoFocus(false)
 	eb:IsMultiLine(false)
-    eb:SetMaxLetters(30)
-    eb:SetPoint("LEFT", label, "RIGHT", x_padding, 0)
+	--eb:SetMaxLetters(30)
+	eb:SetPoint("LEFT", label, "RIGHT", x_padding, 0)
 			
 	local function rank_filter_getter()
 		eb:ClearFocus()
@@ -96,7 +97,7 @@ local function create_text_options(parent_frame, anchor_frame, y_offset)
 		WHODIS_NS.SLASH["rank-filter"].func(eb:GetText())
 	end
 	
-    eb:SetScript("OnEnterPressed", rank_filter_setter)
+	eb:SetScript("OnEnterPressed", rank_filter_setter)
 		
 	local btn = CreateFrame("Button", nil, parent_frame, "UIPanelButtonTemplate")
 	btn:SetPoint("LEFT", eb, "RIGHT", x_padding, 0)
@@ -105,14 +106,133 @@ local function create_text_options(parent_frame, anchor_frame, y_offset)
 	
 	btn:SetScript("OnClick", rank_filter_setter)
 
-	return eb
+	return label
 end
+
+
+local function whodis_create_note_setter(parent_frame, anchor_frame, y_offset)
+	
+	local x_padding = 20
+	
+	local label = parent_frame:CreateFontString(nil , "BORDER", "GameFontNormal")
+	label:SetJustifyH("LEFT")
+	label:SetPoint("TOPLEFT", anchor_frame, "BOTTOMLEFT", 0, y_offset)
+	label:SetText("Set Custom Note")
+	
+	local description = parent_frame:CreateFontString(nil , "BORDER", "GameFontWhite")
+	description:SetJustifyH("LEFT")
+	description:SetPoint("TOPLEFT", label, "BOTTOMLEFT", 0, 0)
+	description:SetText(WHODIS_NS.SLASH["set"].help)
+	
+	
+	local name_eb = CreateFrame("EditBox", nil, parent_frame, "InputBoxTemplate")
+	name_eb:SetSize(200, 22)
+	name_eb:SetAutoFocus(false)
+	name_eb:IsMultiLine(false)
+	--name_eb:SetMaxLetters(30)
+	name_eb:SetPoint("TOPLEFT", description, "BOTTOMLEFT", 0, 0)
+	
+	local note_eb = CreateFrame("EditBox", nil, parent_frame, "InputBoxTemplate")
+	note_eb:SetSize(200, 22)
+	note_eb:SetAutoFocus(false)
+	note_eb:IsMultiLine(false)
+	--note_eb:SetMaxLetters(30)
+	note_eb:SetPoint("LEFT", name_eb, "RIGHT", x_padding, 0)
+	
+		
+	name_eb:SetScript("OnEscapePressed",
+		function()
+			name_eb:ClearFocus()
+		end
+	);
+
+	name_eb:SetScript("OnTabPressed",
+		function(self)
+			name_eb:ClearFocus()
+			note_eb:SetFocus()
+		end
+	);
+	
+	note_eb:SetScript("OnEscapePressed",
+		function()
+			note_eb:ClearFocus()
+		end
+	);
+	
+	
+	local function note_setter()
+		name_eb:ClearFocus()
+		note_eb:ClearFocus()
+		WHODIS_NS.SLASH["set"].func((name_eb:GetText() or "") .. " " .. (note_eb:GetText() or ""))
+	end
+	
+	note_eb:SetScript("OnEnterPressed", note_setter)
+
+		
+	local btn = CreateFrame("Button", nil, parent_frame, "UIPanelButtonTemplate")
+	btn:SetPoint("LEFT", note_eb, "RIGHT", x_padding, 0)
+	btn:SetText("Set")
+	btn:SetWidth(100)
+	
+	btn:SetScript("OnClick", note_setter)
+
+	return name_eb
+end
+
+
+    local FramePool={};
+     
+    local function FramePool_Acquire()
+        return table.remove(FramePool) or CreateFrame("Frame");--   Returns a frame if there is one in the pool or creates one if empty
+    end
+     
+    local function FramePool_Release(frame)
+        table.insert(frame);--  Stores frame in pool
+    end
+	
+
+local function whodis_create_note_list(parent_frame, anchor_frame, y_offset)
+	
+	sf = CreateFrame("ScrollFrame", nil, parent_frame, "UIPanelScrollFrameTemplate")
+	sf:SetPoint("TOPLEFT", anchor_frame, "BOTTOMLEFT", 0, y_offset)
+	sf:SetPoint("BOTTOMRIGHT", -45, 20)
+
+	local eb = CreateFrame("EditBox", nil, sf)
+	eb:SetMultiLine(true)
+	eb:SetFontObject(ChatFontNormal)
+	eb:SetWidth(600)
+	eb:SetAutoFocus(false)
+	eb:EnableMouse(false)
+	eb:SetText("bleh")
+	eb:SetCursorPosition(0)
+		
+	local function list_gen()
+			
+		local roster_list = ""
+		
+		for name, roster_info in pairs(WHODIS_ADDON_DATA_CHAR.ROSTER) do
+			local rank, class, note = unpack(roster_info)
+			roster_list = roster_list .. "Name: " .. name .. " || Custom: " .. tostring(rank == "n/a") .. " || Note: " .. note .. "\n"
+		end
+		
+		eb:SetText(roster_list)
+	end
+	
+	eb:SetScript("OnShow", list_gen) -- ensure the list is rebuilt after opening and closing the window
+	
+	sf:SetScrollChild(eb)
+
+	return sf
+end
+
 
 -- Settings frame must created in a function as its children rely on data structures not present until after the addon initialises
 function WHODIS_NS.create_settings_frame()
 
+	local display_name = "Who Dis" -- ADDON_NAME is not formatted with spaces
+
 	local settings_frame = CreateFrame("Frame")
-	settings_frame.name = ADDON_NAME
+	settings_frame.name = display_name
 	
 	local x_offset = 20
 	local y_offset = -20
@@ -121,21 +241,25 @@ function WHODIS_NS.create_settings_frame()
 	local title_label = settings_frame:CreateFontString(nil , "BORDER", "GameFontNormalLarge")
 	title_label:SetJustifyH("LEFT")
 	title_label:SetPoint("TOPLEFT", settings_frame, "TOPLEFT", x_offset, y_offset)
-	title_label:SetText(ADDON_NAME)
+	title_label:SetText(display_name)
 	
 	local account_opts_label = settings_frame:CreateFontString(nil , "BORDER", "GameFontNormal")
 	account_opts_label:SetJustifyH("LEFT")
 	account_opts_label:SetPoint("TOPLEFT", title_label, "TOPLEFT", 0, y_section_padding)
-	account_opts_label:SetText("Per account settings:")
+	account_opts_label:SetText("Per Account Settings")
 
-	last_bool_opt = create_bool_options(settings_frame, account_opts_label, y_offset)
+	local bool_opt_anchor = whodis_create_bool_options(settings_frame, account_opts_label, y_offset)
 	
 	local char_opts_label = settings_frame:CreateFontString(nil , "BORDER", "GameFontNormal")
 	char_opts_label:SetJustifyH("LEFT")
-	char_opts_label:SetPoint("TOPLEFT", last_bool_opt, "TOPLEFT", 0, y_section_padding)
-	char_opts_label:SetText("Per character settings:")
+	char_opts_label:SetPoint("TOPLEFT", bool_opt_anchor, "TOPLEFT", 0, y_section_padding)
+	char_opts_label:SetText("Per Character Settings")
 	
-	last_text_opt = create_text_options(settings_frame, char_opts_label, y_offset)
+	local text_opt_anchor = whodis_create_text_options(settings_frame, char_opts_label, y_offset)
+	
+	local note_setter_anchor = whodis_create_note_setter(settings_frame, text_opt_anchor, y_section_padding)
+	
+	local note_list_anchor = whodis_create_note_list(settings_frame, note_setter_anchor, y_section_padding)
 
 	InterfaceOptions_AddCategory(settings_frame)
 	
